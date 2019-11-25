feffc7fdc37128e8e5a116d6575bd28c9503c70e
EstevanBR <estevan@bluerocket.us>
2019-11-25 01:38:01 -0800

    0.0.1

---
+++ AAII.framework/Headers/AAII.h
@@ -0,0 +1,19 @@
+//
+//  AAII.h
+//  AAII
+//
+//  Created by Estevan Hernandez on 11/24/19.
+//  Copyright © 2019 Estevan Hernandez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AAII.
+FOUNDATION_EXPORT double AAIIVersionNumber;
+
+//! Project version string for AAII.
+FOUNDATION_EXPORT const unsigned char AAIIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAII/PublicHeader.h>
+
+
